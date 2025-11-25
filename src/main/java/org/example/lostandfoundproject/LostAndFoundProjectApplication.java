bae3ed48ae10f530215049648782ee5ceb23cc20
VictorIrmand <vini0003@stud.kea.dk>
2025-11-25 10:26:06 +0100

    Initial commit

---
+++ src/main/java/org/example/lostandfoundproject/LostAndFoundProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.lostandfoundproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostAndFoundProjectApplication.class, args);
+    }
+
+}
